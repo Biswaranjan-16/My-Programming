c1f43cfaf0ffe1c160bfb0f699a473caf339c669
=Biswaranjan <biswaranjanb627@gmail.com>
2024-04-24 21:36:13 +0530

    24/04/2024

---
+++ String/String1.java
@@ -0,0 +1,50 @@
+package String;
+import java.util.HashSet;
+import java.util.Set;
+
+
+class Main {
+
+ public static void allInterleavings(String res, String P, String Q, Set out) {
+     
+     if (P.length() == 0 && Q.length() == 0) {
+         out.add(res);
+         return;
+     }
+     
+     if (P.length() > 0) {
+         allInterleavings(res + P.charAt(0), P.substring(1), Q, out);
+     }
+ 
+     
+     if (Q.length() > 0) {
+         allInterleavings(res + Q.charAt(0), P, Q.substring(1), out);
+     }
+ }
+
+ 
+ public static void main(String[] args) {
+     
+     String P = "WX";
+     String Q = "YZ";
+
+     
+     System.out.println("The given strings are: " + P + "  " + Q);
+     System.out.println("The interleavings strings are: ");
+
+     
+     Set<String> out = new HashSet<>();
+
+     
+     allInterleavings("", P, Q, out);
+
+     
+     out.stream().forEach(System.out::println);
+ }
+
+
+public void reverseEachWordInString(String strGiven) {
+	
+	
+}
+}
