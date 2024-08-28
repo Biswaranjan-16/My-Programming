88d8ca9f7aed017140f2ee75301017a7f2d21469
=Biswaranjan <biswaranjanb627@gmail.com>
2024-08-28 09:49:59 +0530

    28/07/2024

--- StreamAPI/EmployeeDemo.java
+++ StreamAPI/EmployeeDemo.java
@@ -4,12 +4,12 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
-class Employee{
+class Employee2{
 	int age;
 	double salary;
 	String name;
 	
-	public Employee(String name,int age,double salary) {
+	public Employee2(String name,int age,double salary) {
 		this.name=name;
 		this.age=age;
 		this.salary=salary;
@@ -18,12 +18,12 @@ class Employee{
 public class EmployeeDemo {
 	public static void main(String[] args) {
 
-Employee e1=new Employee("Biswa", 23, 55000.00);
-Employee e2=new Employee("tiku", 33, 35000.00);
-Employee e3=new Employee("miku", 27, 65000.00);
-Employee e4=new Employee("subhu", 29, 45000.00);
+Employee2 e1=new Employee2("Biswa", 23, 55000.00);
+Employee2 e2=new Employee2("tiku", 33, 35000.00);
+Employee2 e3=new Employee2("miku", 27, 65000.00);
+Employee2 e4=new Employee2("subhu", 29, 45000.00);
 
-Stream<Employee> st=Stream.of(e1,e2,e3,e4);
+Stream<Employee2> st=Stream.of(e1,e2,e3,e4);
 
 	}
 }
