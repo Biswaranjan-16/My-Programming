bcc778361615a2263b6b6c35cd0c10b6c21edf67
=Biswaranjan <biswaranjanb627@gmail.com>
2024-10-23 15:45:51 +0530

    23/10/2024

--- Accenture/SquarePlots.java
+++ Accenture/SquarePlots.java
@@ -6,18 +6,18 @@ public class SquarePlots {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Input: Total number of plots
+       
         System.out.print("Enter the total number of plots: ");
         int n = scanner.nextInt();
 
-        // Input: Areas of the plots
+        
         System.out.println("Enter the areas of the plots :");
         int[] areas = new int[n];
         for (int i = 0; i < n; i++) {
             areas[i] = scanner.nextInt();
         }
 
-        // Count square-shaped plots
+        
         int squareCount = 0;
         for (int area : areas) {
             int sqrt = (int) Math.sqrt(area);
@@ -26,7 +26,6 @@ public class SquarePlots {
             }
         }
 
-        // Output: Number of square-shaped plots
         System.out.println("Number of square-shaped plots: " + squareCount);
     }
 }
--- SelectionSort/First.java
+++ SelectionSort/First.java
@@ -1,25 +1,26 @@
 package SelectionSort;
 
 public class First {
-	public static void printArray (int arr[]) {
-		for(int i=0;i<arr.length;i++) {
-			System.out.print(arr[i]+" ");
+	public static void printArray(int arr[]) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
 		}
 		System.out.println();
 	}
+
 	public static void main(String[] args) {
-int arr[]= {1,3,6,9,7,5,8};
-for(int i=0;i<arr.length-1;i++) {
-	int smallest=i;
-	for (int j=i+1;j<arr.length;j++) {
-		if(arr[smallest]>arr[j]) {
-			smallest=j;
+		int arr[] = { 1, 3, 6, 9, 7, 5, 8 };
+		for (int i = 0; i < arr.length - 1; i++) {
+			int smallest = i;
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[smallest] > arr[j]) {
+					smallest = j;
+				}
+			}
+			int temp = arr[smallest];
+			arr[smallest] = arr[i];
+			arr[i] = temp;
 		}
+		printArray(arr);
 	}
-	int temp=arr[smallest];
-	arr[smallest]=arr[i];
-	arr[i]=temp;
-  }
-     printArray(arr);
-     }
 }
