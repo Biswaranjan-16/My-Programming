bab27fd42d57b648cd8fe44fc007e6ca67f2e93d
=Biswaranjan <biswaranjanb627@gmail.com>
2024-12-27 23:40:58 +0530

    27/12/2024

---
+++ Star/Star14.java
@@ -0,0 +1,18 @@
+package Star;
+
+public class Star14 {
+
+	public static void main(String[] args) {
+		for(int i=0;i<=5;i++) {
+			for(int j=1;j<=i;j--) {
+				System.out.println(" ");
+			}
+			for(int k=1;k<=(1*2);k++) {
+				System.out.println("*");
+			}
+			System.out.println(" ");
+		}
+
+	}
+
+}
